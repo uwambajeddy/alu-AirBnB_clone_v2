0a48cdc2be7bc844239f9f55d0154a508c3433a3
Wakhi-Ken <w.dlamini@alustudent.com>
2025-02-08 08:13:55 +0200

    o-web

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,37 +1,21 @@
 #!/usr/bin/python3
-"""scr_ipt that gen_erates a .tgz ar_chive from the cont_ents of the web_static
-fol_der of your AirBnB Clone repo, using the func__tion do_pack.
-"""
-from datetime import datetime
 from fabric.api import local
-import os
+from time import strftime
 
 
 def do_pack():
-    """ gene__rates a .tgz arc_hive from the con_tents of the web_static
-
-    All files in the folder web_static must be added to the final archive.
-    All arc_hives must be sto_red in the folder versions.
-    The name of the archive created must be:
-        web_static_<year><month><day><hour><minute><second>.tgz
-    The function do_pack must return the archive path if the arc_hive has
-    been correctly gen_erated. Otherwise, it shou_ld return N_one.
+    """
+    Generates a .tgz file from the contents of the web_static folder
 
     Returns:
-        fabric.oper_ations._AttributeString: arch_ive path.
+        str: The file path of the generated .tgz file if successful else None.
     """
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
 
-    # create folder versions if it doesn’t exist
-    local("mkdir -p versions")
-
-    # extract the contents of a tar archive
-    result = local("tar -czvf versions/web_static_{}.tgz web_static"
-                   .format(now))
-    if result.failed:
+    date_time = strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(date_time)
+    try:
+        local("mkdir -p versions")
+        local("tar -zcvf {} web_static".format(file_name))
+        return file_name
+    except Exception as err:
         return None
-    else:
-        return result
-
-if __name__ == "__main__":
-    do_pack()
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,80 +1,46 @@
 #!/usr/bin/python3
+"""Deploy web static to different servers"""
+import re
+from fabric.context_managers import cd
+from fabric.api import env, put, run, sudo
+from os.path import join, exists, splitext
 
-import os.path
-from fabric.api import *
-from fabric.operations import run, put
-from datetime import datetime
 
-
-env.hosts = ['172.29.16.1', '192.168.1.3']
 env.user = "ubuntu"
-
-
-def do_pack():
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-
-    # create folder versions if it doesn’t exist
-    local("mkdir -p versions")
-
-    # extract the contents of a tar archive
-    result = local("tar -czvf versions/web_static_{}.tgz web_static"
-                   .format(now))
-    if result.failed:
-        return None
-    else:
-        return result
+env.hosts = ["54.242.215.110", "34.229.154.33"]
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """distributes an archive to your web servers.
-
-    Args:
-        archive_path (string): path to archive
-
-    Returns:
-        Boolean: whether the archive is distributed or not
     """
-    if not os.path.exists(archive_path):
+    Deploy a compressed archive to a remote server.
+    Args:
+        archive_path (str): The path to the compressed archive.
+    Returns:
+        bool: True if the deployment is successful, False otherwise.
+    """
+
+    if not exists(archive_path):
         return False
-    # Uncompress the archive to the folder,
-    # /data/web_static/releases/<archive filename without extension>
-    # on the web server
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
 
     try:
-        # upload the archive to the /tmp/ directory of the web server
-        put(archive_path, "/tmp/{}".format(file_name))
+        put(archive_path, "/tmp/")
+        file_name = re.search(r'[^/]+$', archive_path).group(0)
+        deploy_path = join("/data/web_static/releases/",
+                           splitext(file_name)[0])
+        sudo("mkdir -p {}".format(deploy_path))
 
-        # Create new directory for release
-        run("mkdir -p {}".format(folder_path))
+        sudo("tar -xzf /tmp/{} -C {}".format(file_name, deploy_path))
 
-        # Untar archive
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        with cd(deploy_path):
+            sudo("mv web_static/* .")
+            sudo("rm -rf web_static")
 
-        # Delete the archive from the web server
-        run("rm -rf /tmp/{}".format(file_name))
+        sudo("rm /tmp/{}".format(file_name))
+        sudo("rm -rf /data/web_static/current")
 
-        # Move extraction to proper directory
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        sudo('ln -sf {} /data/web_static/current'.format(deploy_path))
+    except Exception as err:
+        return False
 
-        # Delete first copy of extraction after move
-        run("rm -rf {}web_static".format(folder_path))
-
-        # Delete the symbolic link /data/web_static/current from the web server
-        run("rm -rf /data/web_static/current")
-
-        # Create new the symbolic link /data/web_static/current on web server,
-        # linked to the new version of your code,
-        # (/data/web_static/releases/<archive filename without extension>
-        run("ln -s {} /data/web_static/current".format(folder_path))
-
-        print('New version deployed!')
-        success = True
-
-    except Exception:
-        success = False
-        print("Could not deploy")
-    return success
+    return True
